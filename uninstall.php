9e54955c86b7b0629fbc63e395c8757d9c21122a
Sahil7868 <32764015+Sahil7868@users.noreply.github.com>
2018-09-24 18:21:43 +0530

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -15,6 +15,7 @@ if(! defined('WP_UNINSTALL_PLUGIN'))
 // Access database via sql
 global $wpdb;
 
-$wpdb->query("DELETE FROM wp_posts WHERE post_type = 'sw_images'");
+$wpdb->query("DELETE FROM wp_posts WHERE post_type = 'sw2_images'");
+$wpdb->query("DELETE FROM wp_term_taxonomy WHERE taxonomy = 'sw_category'");
 $wpdb->query("DELETE FROM wp_postmeta WHERE post_id NOT IN(SELECT id FROM wp_posts)" );
 $wpdb->query("DELETE FROM wp_term_relationships WHERE object_id NOT IN(SELECT id FROM wp_posts)" );
